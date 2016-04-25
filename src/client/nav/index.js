e3b6e76a8544173d5d6ea347f8dab6e7ae69fce9
glopezep <guillermolopez2529@gmail.com>
2016-04-25 16:39:53 -0400

    Gestos para el menu agregado

--- src/client/nav/index.js
+++ src/client/nav/index.js
@@ -1,5 +1,15 @@
 import $ from 'jquery'
+import Hammer from 'hammerjs'
+import { changeState } from '../lib'
 
 var $nav = $('.Nav')
+var $appContainer = $('.App-container')
+
+var hammerPanel = new Hammer($appContainer)
+var sidePanel = new Hammer($nav)
+
+hammerPanel.on('swiperight', () => {
+  changeState(null, sidePanel)
+})
 
 export default $nav
